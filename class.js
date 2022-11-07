a59c677bbb8558233765076b31bba60f15095b35
Iwanplayground <backenddeviwan@gmail.com>
2022-11-07 15:54:40 +0700

    menambahkan class mob baru

--- class.js
+++ class.js
@@ -12,3 +12,20 @@ class Bocah {
 let bocah_baru = new Bocah("ujang", 10);
 console.log(bocah_baru);
 console.log(Bocah.getPoin());
+
+console.log("----------------------BATAS----------------------------");
+
+//_____INI BUAT MSLH BERIKUTNYA_____
+
+class Mobil {
+  constructor(mfr, warna, suspensi) {
+    this.mfr = mfr;
+    this.warna = warna;
+    this.suspensi = suspensi;
+    this.mesinIdup = false;
+  }
+}
+
+let civicIwan = new Mobil("honda", "item", "matic");
+
+console.log(`mobil iwan itu keluaran ${civicIwan.mfr} dengan warna ${civicIwan.warna}`);
